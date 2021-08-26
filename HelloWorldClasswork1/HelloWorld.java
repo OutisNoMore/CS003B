2761e3ccf6af5bb30314309f0b1dd72ceabdb0ba
OutisNoMore <OutisNoMore@protonmail.com>
2021-08-25 18:36:07 -0700

    First Program, basic Hello World

---
+++ HelloWorldClasswork1/HelloWorld.java
@@ -0,0 +1,17 @@
+/**
+ * Author: Jaemok C. Lee
+ * Date: 08/29/2021
+ *
+ **/
+
+/**
+ * Basic Hello World program
+ * Makes sure that environment is set up properly
+ **/
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
+}
+
+
