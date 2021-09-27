7203c967bc3a2892e85fbd5ee22d3e9859acdd45
OutisNoMore <OutisNoMore@protonmail.com>
2021-09-27 16:23:56 -0700

    finish assignment

--- CashRegisterClassReview/CashRegister.java
+++ CashRegisterClassReview/CashRegister.java
@@ -2,12 +2,20 @@
 // A new object that represents a cash register that can process transactions
 //
 // Professor: Garen Kutukian
+// Class: CS003B 
 // Author: Jaemok C. Lee
-// Date: 09/13/2021
+// Date: 09/25/2021
+
+import java.util.Date; 
+
 public class CashRegister{
 	private static double allCashRegistersTotals; // total amount processed by all registers
-	private double cashRegisterTotal;						  // total amount processed by single register object
-	private int itemCount; 												// total number of items processed
+	private static int allCashRegistersItems;     // total number of items sold on all registers
+
+	private double cashRegisterTotal;             // total amount processed by single register object
+	private int itemCount;                        // total number of items processed
+
+	private Date currentDate;                     // current date
 
 	/**
 	 * Constructor - initializes new CashRegister object with values initialized to 0
@@ -17,6 +25,9 @@ public class CashRegister{
 		itemCount = 0;
 		cashRegisterTotal = 0.0;
 		allCashRegistersTotals = 0.0;
+		allCashRegistersItems = 0;
+
+		currentDate = new Date();
 	}
 
 	/**
@@ -27,7 +38,8 @@ public class CashRegister{
 	public void addItem(double price){
 		itemCount = itemCount + 1;
 		cashRegisterTotal = cashRegisterTotal + price;
-		allCashRegistersTotals = allCashRegistersTotals - price;
+		allCashRegistersTotals = allCashRegistersTotals + price;
+		allCashRegistersItems = allCashRegistersItems + 1;
 	}
 
 	/**
@@ -35,18 +47,16 @@ public class CashRegister{
 	 * @param price
 	 * @return void
 	 **/
-	public boolean removeItem(double price){
+	public void removeItem(double price){
 		// can't remove object if there aren't any in the first place
 		if(itemCount > 0){
 			itemCount = itemCount - 1;
 			cashRegisterTotal = cashRegisterTotal - price;
 			allCashRegistersTotals = allCashRegistersTotals - price;
-			return true;
+			allCashRegistersItems = allCashRegistersItems - 1;
 		}
-
-		return false;
 	}
-	
+
 	/**
 	 * Mutator - resets all instance variables to 0
 	 * @param NULL
@@ -55,7 +65,15 @@ public class CashRegister{
 	public void clear(){
 		itemCount = 0;
 		cashRegisterTotal = 0.0;
-		allCashRegistersTotals = 0.0;
+	}
+
+	/**
+	 * Mutator - updates date to the current date
+	 * @param NULL
+	 * @return void
+	 **/
+	public void newDay(){
+		currentDate = new Date();
 	}
 
 	/**
@@ -83,5 +101,32 @@ public class CashRegister{
 	 **/
 	public double getAllCashRegisterTotal(){
 		return allCashRegistersTotals;
+	}
+
+	/**
+	 * Accessor - returns total items sold by all registers
+	 * @param NULL
+	 * @return int
+	 **/
+	public int getAllCashRegisterItem(){
+		return allCashRegistersItems;
+	}
+
+	/**
+	 * Accessor - returns a summary of the day's sales as a String
+	 * @param NULL
+	 * @return String
+	 **/
+	public String getEndOfDayReport(){
+		String output;
+		output = "------------------Start Report-------------------\n\n";
+		output = output + "Date: " + currentDate + "\n";
+		output = output + "Total Items Sold: " + itemCount + "\n";
+		output = output + "Cash Register Total: " + cashRegisterTotal + "\n";
+		output = output + "All Cash Register Total: " + allCashRegistersTotals + "\n";
+		output = output + "All Cash Register Items Sold: " + allCashRegistersItems + "\n\n";
+		output = output + "--------------------End of Report--------------------\n";
+
+		return output;
 	}
 }
--- CashRegisterClassReview/UsingCashRegister.java
+++ CashRegisterClassReview/UsingCashRegister.java
@@ -1,15 +1,71 @@
+import java.util.Date;
+
 public class UsingCashRegister{
+
+	public static void Wait(double seconds){
+		Date baseTime = new Date();
+		while(((new Date()).getTime() - baseTime.getTime()) < seconds*1000);
+	}
+
 	public static void main(String[] args){
 		CashRegister testRegister1 = new CashRegister();
 		CashRegister testRegister2 = new CashRegister();
 
 		testRegister1.addItem(4.99);
+		System.out.println("Purchasing item with price of 4.99");
 		testRegister1.addItem(1.95);
-		System.out.println(testRegister1.getCashRegisterTotal()); // should be 6.94
-		System.out.println(testRegister1.getCount()); // should be 2
+		System.out.println("Purchasing item with price of 1.95");
+		System.out.println("Printing Cash Register Total...");
+		System.out.println("Expecting 6.94 --- Received " + testRegister1.getCashRegisterTotal()); // should be 6.94
+		System.out.println("Printing total items sold");
+		System.out.println("Expecting 2 ---  Received " + testRegister1.getCount());               // should be 2
 
 		testRegister1.removeItem(0.50);
-		System.out.println(testRegister1.getCashRegisterTotal()); // should be 6.44
-		System.out.println(testRegister1.getCount()); // should be 1
+		System.out.println("Removing 0.50 from total");
+
+		System.out.println("Printing Cash Register Total...");
+		System.out.println("Expecting 6.44 ---  Received " + testRegister1.getCashRegisterTotal());    // should be 6.44
+		System.out.println("Printing Cash Register Total items sold...");
+		System.out.println("Expecting 1 ---  Received " + testRegister1.getCount());                   // should be 1
+		System.out.println("Printing all Cash Register Total...");
+		System.out.println("Expecting 6.44 ---  Received " + testRegister1.getAllCashRegisterTotal()); // should be 6.44
+		System.out.println("Printing all Cash Register Total items sold...");
+		System.out.println("Expecting 1 ---  Received " + testRegister1.getAllCashRegisterItem());     // should be 1
+		System.out.println("Printing end of day report for first cash register");
+		System.out.println(testRegister1.getEndOfDayReport());
+
+		testRegister2.addItem(15.95);
+		System.out.println("Purchasing item with price 15.95");
+		testRegister2.addItem(22.99);
+		System.out.println("Purchasing item with price 22.99");
+		testRegister2.addItem(1.99);
+		System.out.println("Purchasing item with price 1.99");
+		testRegister2.addItem(33.95);
+		System.out.println("Purchasing item with price 33.95");
+
+		System.out.println("Printing Cash Register Total...");
+		System.out.println("Expecting 74.88 ---  Received " + testRegister2.getCashRegisterTotal()); // should be 74.88
+		System.out.println("Printing Cash Register Total items sold...");
+		System.out.println("Expecting 4 ---  Received " + testRegister2.getCount());                 // should be 4
+
+		testRegister2.removeItem(1.99);
+		System.out.println("Removing 1.99 from Cash Register Total...");
+
+		System.out.println("Printing Cash Register Total...");
+		System.out.println("Expecting 72.89 ---  Received " + testRegister2.getCashRegisterTotal());    // should be 72.89
+		System.out.println("Printing Cash Register Total items sold...");
+		System.out.println("Expecting 3 ---  Received " + testRegister2.getCount());                    // should be 3
+		System.out.println("Printing Cash Register Total...");
+		System.out.println("Expecting 79.33 ---  Received " + testRegister2.getAllCashRegisterTotal()); // should be 79.33
+		System.out.println("Test: " + CashRegister.getAllCashRegisterTotal());
+		System.out.println("Printing Cash Register Total items sold...");
+		System.out.println("Expecting 4 ---  Received " + testRegister2.getAllCashRegisterItem());      // should be 4
+		System.out.println(testRegister2.getEndOfDayReport());
+
+		System.out.printf("Waiting %.2f seconds\n", 5.00);
+		Wait(5); // wait 10 seconds
+		System.out.println("Updating to current time");
+		testRegister2.newDay();//	check that new date changed to current date and time
+		System.out.println(testRegister2.getEndOfDayReport());
 	}
 }
